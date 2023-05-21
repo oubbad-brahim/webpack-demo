2a6f0fff75d71222e7776de295ad6549aa95ae42
Brahim Oubbad <91596125+BrahimOubbad@users.noreply.github.com>
2023-05-21 15:59:41 +0000

    Output management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from "lodash";
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js'
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], ' ');
@@ -14,6 +16,11 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
